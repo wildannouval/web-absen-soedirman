62f7954e23ed92d64916577f28abaaa92bf2d8d6
wildannouval <senangkucing@gmail.com>
2022-11-24 01:51:34 +0700

    Manage file

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'webabsensi';
-
-$conn = mysqli_connect($host,$user,$pass,$db) or die ('Cant connect to database');
-?>
